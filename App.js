9257f41779e95737ec2e8c4ce963d88a30bba307
shariThasneem <sharibathasneem@gmail.com>
2023-11-21 13:12:37 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,125 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, Platform, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Home, Search, Settings, Notification } from "./screen";
+import { Entypo } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 
+const Tab = createBottomTabNavigator();
+const screenOptions = {
+	tabBarShowLabel: false,
+
+	tabBarStyle: {
+		position: "absolute",
+		bottom: 20,
+		right: 0,
+		left: 0,
+		elevation: 0,
+		height: 75,
+		width: "90%",
+		backgroundColor: "#1D1F24",
+		borderRadius: 100,
+	},
+};
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer
+			style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+			<Tab.Navigator screenOptions={screenOptions}>
+				<Tab.Screen
+					name="Home"
+					component={Home}
+					options={{
+						tabBarIcon: ({ focused }) => {
+							return (
+								<View
+									style={{
+										alignItems: "center",
+										justifyContent: "center",
+									}}>
+									<Entypo
+										name="home"
+										size={28}
+										color={focused ? "#78531E" : "#676D75"}
+									/>
+								</View>
+							);
+						},
+						tabBarActiveBackgroundColor: "#EFB875",
+						tabBarItemStyle: {
+							borderRadius: 50,
+						},
+					}}
+				/>
+				<Tab.Screen
+					name="Search"
+					component={Search}
+					options={{
+						tabBarIcon: ({ focused }) => {
+							return (
+								<View
+									style={{ alignItems: "center", justifyContent: "center" }}>
+									<FontAwesome
+										name="search"
+										size={28}
+										color={focused ? "#78531E" : "#676D75"}
+									/>
+								</View>
+							);
+						},
+						tabBarActiveBackgroundColor: "#EFB875",
+						tabBarItemStyle: {
+							borderRadius: 50,
+						},
+					}}
+				/>
+				<Tab.Screen
+					name="Notification"
+					component={Notification}
+					options={{
+						tabBarIcon: ({ focused }) => {
+							return (
+								<View
+									style={{ alignItems: "center", justifyContent: "center" }}>
+									<Ionicons
+										name="notifications"
+										size={28}
+										color={focused ? "#78531E" : "#676D75"}
+									/>
+								</View>
+							);
+						},
+						tabBarActiveBackgroundColor: "#EFB875",
+						tabBarItemStyle: {
+							borderRadius: 50,
+						},
+					}}
+				/>
+				<Tab.Screen
+					name="Settings"
+					component={Settings}
+					options={{
+						tabBarIcon: ({ focused }) => {
+							return (
+								<View
+									style={{ alignItems: "center", justifyContent: "center" }}>
+									<Feather
+										name="settings"
+										size={28}
+										color={focused ? "#78531E" : "#676D75"}
+									/>
+								</View>
+							);
+						},
+						tabBarActiveBackgroundColor: "#EFB875",
+						tabBarItemStyle: {
+							borderRadius: 50,
+						},
+					}}
+				/>
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screen/index.js
@@ -0,0 +1,6 @@
+import Home from "./home";
+import Search from "./search";
+import Settings from "./settings";
+import Notification from "./notification";
+
+export { Home, Search, Settings, Notification };
